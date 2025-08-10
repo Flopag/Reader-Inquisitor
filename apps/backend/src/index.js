562b5af9ef37c07a92c0962e435324cad260fa3a
Flopag <58123163+Flopag@users.noreply.github.com>
2025-08-10 13:28:22 +0200

    deployment configuration files, backend

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! (yep, thats a test)')
 })
 
 app.listen(port, () => {
