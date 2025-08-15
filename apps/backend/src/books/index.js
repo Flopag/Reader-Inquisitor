d983cfe2b79cde9506db11edb9ed7d4428d7d3c6
Flopag <58123163+Flopag@users.noreply.github.com>
2025-08-15 18:23:56 +0200

    list all book and remove one

--- apps/backend/src/books/index.js
+++ apps/backend/src/books/index.js
@@ -110,4 +110,74 @@ async (req, res) => {
     }
 });
 
+router.get('/',
+    require('@app/auth/middlewares').at_least_basic,
+async (req, res) => {
+    try {
+        const books = await Book.findAll();
+
+        res.status(200).json({
+            "success": true,
+            "message": `See data for the books list`,
+            "data": books,
+            "error_code": null,
+        });
+    } catch (err) {
+        console.error('[get /books/]: An error occured with the database:', err);
+        res.status(500).json({
+            "success": false,
+            "message": `An error occured with the database`,
+            "data": {},
+            "error_code": null,
+        });
+        return;
+    }
+});
+
+router.delete('/:book_id',
+    require('@app/auth/middlewares').at_least_admin,
+async (req, res) => {
+    const { book_id } = req.params;
+
+    if(!book_id){
+        res.status(400).json({
+            "success": false,
+            "message": `The id of the book is missing`,
+            "data": {},
+            "error_code": null,
+        });
+        return;
+    }
+
+    try {
+        if(!(await Book.findOne({where: {book_id: book_id}}))){
+            res.status(400).json({
+                "success": false,
+                "message": `The book ${book_id} do not exist`,
+                "data": {},
+                "error_code": null,
+            });
+            return;
+        }
+
+        await Book.destroy({where: {book_id: book_id}});
+
+        res.status(200).json({
+            "success": true,
+            "message": `The book ${book_id} has been destroy`,
+            "data": {},
+            "error_code": null,
+        });
+    } catch (err) {
+        console.error('[delete /books/:book_id]: An error occured with the database:', err);
+        res.status(500).json({
+            "success": false,
+            "message": `An error occured with the database`,
+            "data": {},
+            "error_code": null,
+        });
+        return;
+    }
+});
+
 module.exports = router;
