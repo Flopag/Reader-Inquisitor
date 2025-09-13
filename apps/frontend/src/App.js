79d2e45865ee413040c74adc67df244c048a04bf
Flopag <58123163+Flopag@users.noreply.github.com>
2025-09-13 14:03:14 +0200

    add router and navigation

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,8 +1,23 @@
 import Login from '@components/login.js';
-import Profile_page from '@pages/profile'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Profile_page from '@pages/profile';
 
 export function App() {
     return  <>
-                <Login child={<Profile_page />} />
+                <BrowserRouter>
+                    <Login child={
+                        <div>
+                            <nav>
+                                <Link to="/">Home</Link> |{" "}
+                                <Link to="/profile">Profile</Link>
+                            </nav>
+                            <Routes>
+                                <Route path="/" element={<h1>Welcome to reader inquisitor</h1>} />
+                                <Route path="/profile" element={<Profile_page />} />
+                            </Routes>
+                        </div>
+                    } />
+                </BrowserRouter>
             </>;
 }
