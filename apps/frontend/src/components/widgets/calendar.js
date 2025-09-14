5aa66ed5abbb959e2b47ac2469f53c9dbdd68d2c
Flopag <58123163+Flopag@users.noreply.github.com>
2025-09-14 11:34:36 +0200

    header and changing month

--- apps/frontend/src/components/widgets/calendar.js
+++ apps/frontend/src/components/widgets/calendar.js
@@ -2,49 +2,101 @@ import React from 'react';
 import "./css/calendar.css";
 
 class Calendar extends React.Component {
-    render(){
-        const get_calendar_row_html = (first, last, today) => {
-            const cells = [];
-
-            for(let i=0; i < 7; ++i){
-                const is_active = (i < first || i > last) ? false : true;
-                cells.push(<div key={i} className={`Calendar-cell-${is_active} today-${i==today}`} id={`calendar-cell-${i}`}></div>)
-            }
-
-            return cells;
+    constructor(props) {
+        super(props);
+        const today = new Date();
+        this.state = {
+            year: today.getFullYear(),
+            month: today.getMonth() + 1
         };
-        
-        const get_calendar_html = (year, month) => {
-            if(month < 1 || month > 12)
-                return;
+    }
+    
+    get_calendar_row_html(first, last, today){
+        const cells = [];
 
-            const first_day = new Date(year, month-1, 1).getDay();
-            const last_day = new Date(year, month, 0).getDay();
+        for(let i=0; i < 7; ++i){
+            const is_active = (i < first || i > last) ? false : true;
+            cells.push(<div key={i} className={`Calendar-cell-${is_active} today-${i==today}`} id={`calendar-cell-${i}`}></div>)
+        }
 
-            const today = new Date();
-            let totay_dd = -1;
-            if(today.getMonth()+1 == month && today.getFullYear() == year)
-                totay_dd = today.getDate();
+        return cells;
+    }
 
-            const rows = [];
+    get_calendar_html(year, month){
+        if(month < 1 || month > 12)
+            return;
 
-            for(let i=0; i < 5; ++i){
-                let today_day = -1;
+        const first_day = new Date(year, month-1, 1).getDay();
+        const last_day = new Date(year, month, 0).getDay();
 
-                if(totay_dd != -1 && totay_dd+first_day <= (i+1)*7 && totay_dd+first_day > i*7)
-                    today_day = today.getDay();
+        const today = new Date();
+        let totay_dd = -1;
+        if(today.getMonth()+1 == month && today.getFullYear() == year)
+            totay_dd = today.getDate();
 
-                rows.push(<div key={i} className='Calendar-row' id={`calendar-row-${i}`}>{
-                    (i==0) ? get_calendar_row_html(first_day, 7, today_day) :
-                    (i==4) ? get_calendar_row_html(0, last_day, today_day) :
-                    get_calendar_row_html(0, 7, today_day)
-                }</div>)
-            }
+        const rows = [];
 
-            return rows;
+        for(let i=0; i < 5; ++i){
+            let today_day = -1;
+
+            if(totay_dd != -1 && totay_dd+first_day <= (i+1)*7 && totay_dd+first_day > i*7)
+                today_day = today.getDay();
+
+            rows.push(<div key={i} className='Calendar-row' id={`calendar-row-${i}`}>{
+                (i==0) ? this.get_calendar_row_html(first_day, 7, today_day) :
+                (i==4) ? this.get_calendar_row_html(0, last_day, today_day) :
+                this.get_calendar_row_html(0, 7, today_day)
+            }</div>)
+        }
+
+        return rows;
+    }
+
+    to_previous_month(){
+        this.setState((previous_state) => {
+            let { year, month } = previous_state; 
+
+            if(month == 1){
+                year -= 1;
+                month = 12;
+            } else 
+                month -= 1;
+                
+            return { year, month };
+        });
+    }
+
+    to_next_month(){
+        this.setState((previous_state) => {
+            let { year, month } = previous_state; 
+
+            if(month == 12){
+                year += 1;
+                month = 1;
+            } else 
+                month += 1;
+                
+            return { year, month };
+        });
+    }
+
+    render(){
+        const { year, month } = this.state;
+
+        const to_previous_month = () => {
+            this.to_previous_month();
+        };
+
+        const to_next_month = () => {
+            this.to_next_month();
         };
 
         return  <div id="Calendar">
+                    <div className='Calendar-headers'>
+                        <div className={`Calendar-label-cell`} onClick={to_previous_month} style={{cursor: "pointer"}}>prev</div>
+                        <div id="Calendar-title">{month}/{year}</div>
+                        <div className={`Calendar-label-cell`} onClick={to_next_month} style={{cursor: "pointer"}}>Next</div>
+                    </div>
                     <div className='Calendar-row'>
                         <div className={`Calendar-label-cell`}>Su</div>
                         <div className={`Calendar-label-cell`}>Mo</div>
@@ -54,7 +106,7 @@ class Calendar extends React.Component {
                         <div className={`Calendar-label-cell`}>Fr</div>
                         <div className={`Calendar-label-cell`}>Sa</div>
                     </div>
-                    {get_calendar_html(2025, 9)}
+                    {this.get_calendar_html(year, month)}
                 </div>
     }
 }
