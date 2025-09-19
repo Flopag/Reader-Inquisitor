a28f202122b36c213176c85bae67dbe211d33e2d
Flopag <58123163+Flopag@users.noreply.github.com>
2025-09-19 22:07:38 +0200

    make the script available to nodejs

--- apps/bot/src/index.js
+++ apps/bot/src/index.js
@@ -1,6 +1,7 @@
 require('module-alias/register');
 
 const express = require('express');
+const { spawn } = require('child_process');
 
 const app = express();
 const port = process.env.PORT;
@@ -12,6 +13,22 @@ app.use(express.json());
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
+
+app.patch('/check_users', async (req, res) => {
+    const p = spawn('python3', ['./src/scripts/check_users.py']);
+
+    p.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    });
+
+    p.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    });
+
+    p.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    });
+});
 
 app.get('/health', async (req, res) => {
     require('@utils/responses').success(res, `The API is healthy`, {});
--- infra/docker/bot.Dockerfile
+++ infra/docker/bot.Dockerfile
@@ -3,5 +3,12 @@ WORKDIR /home
 COPY apps/bot .
 RUN npm install
 RUN apk add curl
+RUN apk add python3
+RUN apk add py3-pip
+RUN python3 -m venv /opt/venv \
+ && source /opt/venv/bin/activate \
+ && pip install --upgrade pip \
+ && pip install -r requirements.txt
+ENV PATH="/opt/venv/bin:$PATH"
 RUN chmod +x healthcheck.sh
 RUN chmod +x test.sh
